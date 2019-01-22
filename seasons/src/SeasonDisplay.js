82bd5b8dc414a545a9016341e0b807840dac855e
Michael Wilson <info@michaelwilsion.io>
2019-01-22 18:32:04 -0500

    Completed the Structure Apps with Class-Based Components section

---
+++ seasons/src/SeasonDisplay.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SeasonDisplay = () => {
+    return (
+        <div>
+            SeasonDisplay Component
+        </div>
+    );
+}
+
+
+export default SeasonDisplay;
---
+++ seasons/src/index.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+import SeasonDisplay from './SeasonDisplay';
+
+
+class App extends Component {
+    render() {
+
+        window.navigator.geolocation.getCurrentPosition(
+            ( position ) => { console.log( position ) },
+            ( error ) => { console.log( error ) }
+        );
+
+        return(
+            <div>
+                Latitude:
+            </div>
+        );
+    }
+}
+
+
+ReactDOM.render(
+    <App />,
+    document.querySelector( "#root" )
+);
