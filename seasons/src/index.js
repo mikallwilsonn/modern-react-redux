251bd9f561ca14661311800e47d59e7a13f7b834
Michael Wilson <info@michaelwilsion.io>
2019-01-22 20:20:45 -0500

    Completed the State in React Components section of course

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -5,18 +5,34 @@ import SeasonDisplay from './SeasonDisplay';
 
 
 class App extends Component {
-    render() {
+
+    constructor( props ) {
+        super( props );
+
+        this.state = {
+            lat: null,
+            errorMessage: ''
+        }
 
         window.navigator.geolocation.getCurrentPosition(
-            ( position ) => { console.log( position ) },
-            ( error ) => { console.log( error ) }
+            ( position ) => {
+                this.setState({ lat: position.coords.latitude });
+            },
+            ( error ) => {
+                this.setState({ errorMessage: error.message })
+            }
         );
 
-        return(
-            <div>
-                Latitude:
-            </div>
-        );
+    }
+
+    render() {
+        if ( this.state.errorMessage && !this.state.lat  ) {
+            return <div>Error: {this.state.errorMessage}</div>
+        }
+        if ( !this.state.errorMessage && this.state.lat ) {
+            return <div>Latitude: {this.state.lat}</div>
+        }
+        return <div>Loading...</div>;
     }
 }
 
