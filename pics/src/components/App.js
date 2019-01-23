c3028978703466977609a7fc4df536f92b7318b8
Michael Wilson <info@michaelwilsion.io>
2019-01-23 15:16:04 -0500

    Completed the Making API Requests with React section

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,17 +1,27 @@
 import React, { Component } from 'react';
-
+import unsplash from '../api/unsplash';
 import SearchBar from './SearchBar';
+
+
+
 
 class App extends Component {
 
-    onSearchSubmit( term ) {
-        console.log( term );
+    state = { images: [] };
+
+    onSearchSubmit = async ( term ) => {
+        const response = await unsplash.get( 'https://api.unsplash.com/search/photos', {
+            params: { query: term }
+        });
+
+        this.setState({ images: response.data.results });
     }
 
     render() {
         return (
             <div className="ui container" style={{ paddingTop: '25px' }}>
                 <SearchBar onSubmit={this.onSearchSubmit} />
+                Found: {this.state.images.length} images
             </div>
             
         );
