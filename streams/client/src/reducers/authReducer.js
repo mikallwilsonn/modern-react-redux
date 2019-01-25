c495922017b56089fccf747330d1063b60cbc884
Michael Wilson <info@michaelwilsion.io>
2019-01-25 17:06:27 -0500

    Completed Handling User Authentication with React

---
+++ streams/client/src/reducers/authReducer.js
@@ -0,0 +1,16 @@
+import { SIGN_IN, SIGN_OUT } from '../actions/types';
+const INITIAL_STATE = { 
+    isSignedIn: null,
+    user_id: null 
+}
+
+export default ( state = INITIAL_STATE , action ) => {
+    switch ( action.type ) {
+        case SIGN_IN:
+            return {...state, isSignedIn: true, user_id: action.payload};
+        case SIGN_OUT:
+            return {...state, isSignedIn: false, user_id: null};
+        default:
+            return state;
+    }
+}
